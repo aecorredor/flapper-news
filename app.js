f7f425cf0fa4e403774ccf3b898c3434a8d6efb6
Alejandro Corredor <aecorredor93@yahoo.com>
2016-05-09 16:46:59 -0400

    finished backend node implementation

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+var mongoose = require('mongoose');
+require('./models/Posts');
+require('./models/Comments');
+
+mongoose.connect('mongodb://localhost/news');
+
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -18,7 +24,7 @@ app.set('view engine', 'ejs');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -55,6 +61,5 @@ app.use(function(err, req, res, next) {
     error: {}
   });
 });
-
 
 module.exports = app;
