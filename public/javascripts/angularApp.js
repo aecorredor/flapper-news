13b40cd38ea00cbaf49058ae527308272bbb0fa8
Alejandro Corredor <aecorredor93@yahoo.com>
2016-05-11 10:41:45 -0400

    finished upvote comments feature

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -31,6 +31,18 @@ app.factory('posts', ['$http', function($http) {
     });
   };
 
+  o.addComment = function(id, comment) {
+    return $http.post('/posts/' + id + '/comments', comment);
+  };
+
+  o.upvoteComment = function(post, comment) {
+    return $http.put('/posts/' + post._id + '/comments/' + comment._id +
+    '/upvote')
+    .success(function(data) {
+      comment.upvotes += 1;
+    });
+  };
+
   return o;
 }]);
 
@@ -95,12 +107,16 @@ app.controller('PostsCtrl', [
     $scope.post = post;
     $scope.addComment = function() {
       if ($scope.body === '') { return; }
-      $scope.post.comments.push({
+      posts.addComment(post._id, {
         body: $scope.body,
         author: 'user',
-        upvotes: 0
+      }).success(function(comment) {
+        $scope.post.comments.push(comment);
       });
       $scope.body = '';
+    };
+    $scope.incrementUpvotes = function(comment) {
+      posts.upvoteComment(post, comment);
     };
   }
 ]);
